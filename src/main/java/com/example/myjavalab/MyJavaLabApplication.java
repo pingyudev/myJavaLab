af15929e70994db83e7647f1af71ff3fa3d776ce
peteryu <ping.yu.dev@gmail.com>
2025-09-27 09:59:41 +0800

    Initialize Spring Boot project with Maven wrapper and basic structure. Added .gitignore, .gitattributes, and configuration files. Implemented a simple HelloController and application properties.

---
+++ src/main/java/com/example/myjavalab/MyJavaLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.myjavalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myjavalab/MyJavaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myjavalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

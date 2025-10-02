f55e597a64cdab676d24eaefa1ec62bac86c014b
yuping <ping.yu.dev@gmail.com>
2025-10-02 08:53:46 +0800

    new api

--- src/main/java/com/example/myjavalab/controller/HelloController.java
+++ src/main/java/com/example/myjavalab/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.myjavalab.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello world!";
+    }
+
+    @GetMapping("/helloYou")
+    public String helloYou(@RequestParam("username") String username) {
+        return "Hello, " + username;
     }
 }
